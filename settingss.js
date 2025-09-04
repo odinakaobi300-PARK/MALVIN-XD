fbdce7d6662d38c3728eb866f364da5432c11a6e
odinakaobi300-PARK <odinakaobi300@gmail.com>
2025-09-04 16:16:31 +0100

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,6 +1,6 @@
 
 module.exports = {
-  SESSION_ID: "malvin~YmliHTqb#-aE6mYZWS3J-62NRPTftH3wDCQzJGq7sig-q54L9UdE",  // add your Session Id here
+  SESSION_ID: "malvin~w7dQQJya#RM2_lYNaRyxvqSA9_bI6QRH_hYpge68A4eiMK0UNPM0",  // add your Session Id here
   
   OWNER_NUMBER: "2349134519459", // put your phone number here
   
